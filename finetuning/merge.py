93156c32649fc5089c0d39fb9ce0752bc4b58fdb
giannisak <154079765+giannisak@users.noreply.github.com>
2024-06-21 15:40:15 +0300

    Create merge.py

---
+++ finetuning/merge.py
@@ -0,0 +1,21 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftModel
+import torch
+
+# Define model paths and repository names
+hf_adapter_repo = "jak6/results_Phi3_medium_4k"
+base_model_name = "microsoft/Phi-3-medium-4k-instruct"
+compute_dtype = torch.bfloat16
+
+# Load the base model and apply the adapter weights
+model = AutoModelForCausalLM.from_pretrained(base_model_name, trust_remote_code=True, torch_dtype=compute_dtype)
+model = PeftModel.from_pretrained(model, hf_adapter_repo)
+model = model.merge_and_unload()
+
+# Load the tokenizer
+tokenizer = AutoTokenizer.from_pretrained(hf_adapter_repo)
+
+# Save the merged model and tokenizer to the Hugging Face Model Hub
+merged_model_id = "jak6/Phi3_medium_4k_finetuned"
+model.push_to_hub(merged_model_id)
+tokenizer.push_to_hub(merged_model_id)
